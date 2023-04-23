79961cb3fe20a71313c8872a4f418b0f2bc95025
jpclrocha <jpclrocha@gmail.com>
2023-04-23 16:27:27 -0300

    Created class Position

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position a = new Position(3,3);
+        System.out.println(a);
+    }
+}
