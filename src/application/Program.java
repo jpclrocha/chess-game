ac70ca02a804e4ab380f843bfe45b450583b0936
jpclrocha <jpclrocha@gmail.com>
2023-04-23 16:52:48 -0300

    Created chess layer and print board

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,10 +2,11 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Board b = new Board(8,8);
-        System.out.println(b.toString());
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
